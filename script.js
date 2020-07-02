87a4bb8e7a824228c0a165fa87eb516cbde75d58
Marcos <marcosadriano@infojr.com.br>
2020-07-02 18:31:26 -0300

    Estrutura inicial

---
+++ script.js
@@ -0,0 +1,70 @@
+let queue = [
+  {
+    name: "Antonio figueiredo",
+    amount: 3,
+  },
+  {
+    name: "Maria Carolina",
+    amount: 1,
+  },
+  {
+    name: "Jão Vitor",
+    amount: 2,
+  },
+  {
+    name: "Andressa Silva",
+    amount: 27,
+  },
+];
+
+const inputName = document.querySelector(".name");
+const inputAmount = document.querySelector(".amount");
+const button = document.querySelector("button");
+const line = document.getElementById("line");
+
+updateQueue();
+
+setInterval(function(){
+  queue.shift();
+  updateQueue();
+},10000);
+
+button.addEventListener("click", event => {
+
+  queue.push({
+    name: inputName.value,
+    amount: inputAmount.value,
+  });
+
+  if (inputAmount.value == 71) {
+    queue = queue.filter(client => client.amount % 2 ==! 0);
+    console.log("hi");
+  }
+
+  updateQueue();
+  inputName.value = "";
+  inputAmount.value = "";
+
+});
+
+function updateQueue() {
+  line.innerHTML = "";
+
+  for(const client of queue) {
+
+    const name = document.createElement("p");
+    name.classList.add("name");
+    name.innerText = client.name.toUpperCase();
+
+    const amount = document.createElement("p");
+    amount.classList.add("amount");
+
+    if(client.amount == 1) {
+      amount.innerText = "Quantidade: " + client.amount + " pão";
+    } else {
+      amount.innerText = "Quantidade: " + client.amount + " pães";
+    }
+    
+    line.append(name,amount);
+  }
+}
